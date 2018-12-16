fdaf2d90bb8056ddcd2369da1ff6d1f6f5f2057e
Kub <kub@Kub-MacBook-Pro.local>
2018-12-16 13:53:27 +0300

    save changed

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -20,6 +20,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonAlert(_ sender: Any) {
+        //AlertControl tanımlayop başlık ve mesajı belirttik
+        let alertControl = UIAlertController(title: "Warning", message: "This is a warning!", preferredStyle: .alert)
+        //Alert içinde buttonu ekledik.
+        let alertButton = UIAlertAction(title: "Close ", style: .cancel, handler: {(uyari: UIAlertAction)-> Void in
+            
+            
+        })
+        //İkisini birleştirip Ekranfa görüntülenmesini sağladık.
+        alertControl.addAction(alertButton)
+        self.present(alertControl, animated: true, completion: nil)
+    }
+    
 }
 
